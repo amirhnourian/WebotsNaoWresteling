260ad664f148696eb760d102ab6144ff5b1bd48e
Mahdi-95 <79779293+Mahdi-95@users.noreply.github.com>
2023-05-15 17:09:05 +0330

    Update participant.py

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -95,11 +95,11 @@ class Eve (Robot):
             self.fsm.execute_action()
 
     def choose_action(self):
-        #self.motions['Shove'].play()
-        self.motions['Forwards50'].play()
-        #if self.opponent_position.average > -0.4 and self.opponent_position.average < 0.4:
+        self.motions['Shove'].play()
+        #self.motions['Forwards50'].play()
+        if self.opponent_position.average > -0.4 and self.opponent_position.average < 0.4:
             #self.current_motion.set(self.motions['Forwards50'])
-        if self.opponent_position.average < -0.4:
+        elif self.opponent_position.average < -0.4:
             self.current_motion.set(self.motions['TurnLeft'])
         elif self.opponent_position.average > 0.4:
             self.current_motion.set(self.motions['TurnRight'])
