161fbbf010d0562a9428f91a5d9028209facadca
Mahdi-95 <79779293+Mahdi-95@users.noreply.github.com>
2023-05-15 18:37:22 +0330

    Update participant.py

--- controllers/participant/participant.py
+++ controllers/participant/participant.py
@@ -1,53 +1,173 @@
-from controller import Robot
+# Copyright 1996-2023 Cyberbotics Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Demonstrates how to use the camera and gives an image processing example to locate the opponent.
+"""
+
 import sys
-
 sys.path.append('..')
-from utils.motion_library import MotionLibrary
-from math import pi, sin
+from utils.camera import Camera
+from utils.camera_bottom import CameraBottom
+from utils.fall_detection import FallDetection  # David's fall detection is implemented in this class
+from utils.running_average import RunningAverage
+from utils.image_processing import ImageProcessing as IP
+from utils.finite_state_machine import FiniteStateMachine
+from utils.current_motion_manager import CurrentMotionManager
+from utils.border_detection import BorderDetection
+from controller import Robot, Motion
+import cv2
 
-class Wrestler (Robot):
+class Eve (Robot):
+    NUMBER_OF_DODGE_STEPS = 10
+
     def __init__(self):
-    
-        super().__init__()
-        self.motion_library = MotionLibrary()
+        Robot.__init__(self)
+
+        # retrieves the WorldInfo.basicTimeTime (ms) from the world file
         self.time_step = int(self.getBasicTimeStep())
-        self.leds = {'right': self.getDevice("Ears/Led/Right"),
-                     'left':  self.getDevice("Ears/Led/Left")}
-        self.RShoulderPitch = self.getDevice("RShoulderPitch")
-        self.LShoulderPitch = self.getDevice("LShoulderPitch")
-        self.Sonar1 = self.getDevice("Sonar/Left")
-        self.Sonar2 = self.getDevice("Sonar/Right")
-        self.Sonar1.enable(self.time_step)
-        self.Sonar2.enable(self.time_step)
-           
-    def run(self):
-    
-        self.RShoulderPitch.setPosition(0)
-        self.LShoulderPitch.setPosition(0) 
+        self.leds = {'right': self.getDevice("Ears/Led/Right"),'left':  self.getDevice("Ears/Led/Left")}
         
-  
-        ts = 0  
-        F = 3
-              
-        while self.step(self.time_step) != -1:  
-            self.motion_library.play('Forwards')
-       
+        self.fsm = FiniteStateMachine(
+            states=['CHOOSE_ACTION', 'BLOCKING_MOTION'],
+            initial_state='CHOOSE_ACTION',
+            actions={
+                'CHOOSE_ACTION': self.choose_action,
+                'BLOCKING_MOTION': self.pending
+            }
+        )
+        self.camera = Camera(self)
+
+
+        # arm motors for getting up from a side fall
+        self.RShoulderRoll = self.getDevice("RShoulderRoll")
+        self.LShoulderRoll = self.getDevice("LShoulderRoll")
+
+        self.fall_detector = FallDetection(self.time_step, self)
+        self.current_motion = CurrentMotionManager()
+        # load motion files
+        self.motions = {
+            'SideStepLeft': Motion('../motions/SideStepLeftLoop.motion'),
+            'SideStepRight': Motion('../motions/SideStepRightLoop.motion'),
+            'TurnRight': Motion('../motions/TurnRight20.motion'),
+            'TurnLeft': Motion('../motions/TurnLeft20.motion'),
+            'Forwards50': Motion('../motions/ForwardLoop.motion'),
+            'Shove': Motion('../motions/Shove.motion'),
+            'Attack': self.attack()
+        }
+        self.opponent_position = RunningAverage(dimensions=1)
+        self.dodging_direction = 'left'
+        self.counter = 0
+
+        # init for line detection
+        self.border_detector = BorderDetection(self.time_step,self)
+        self.camera_bottom = CameraBottom(self)
+
+        # adjusting head position
+        # HeadPitch
+        self.HeadPitch = self.getDevice("HeadPitch")
+        self.HeadPitch.setPosition(0.25)
+        
+    def run(self):
+        ts = 0 
+        while self.step(self.time_step) != -1:
             ts = (ts + self.time_step)
-            d1 = self.Sonar1.getValue()
-            d2 = self.Sonar2.getValue()
-
             t = ts/1000
-            position = sin(t * 2.0 * pi * F)
-
-            
-            if min(d1,d2) < 0.3:
-                self.RShoulderPitch.setPosition(-position)
-                self.LShoulderPitch.setPosition(position)  
-                                          
             if t > 2.0:
                 self.leds['right'].set(0xff0000)
-                self.leds['left'].set(0xff0000)            
+                self.leds['left'].set(0xff0000)
+                
+            self.opponent_position.update_average(
+                self._get_normalized_opponent_horizontal_position())
+            self.fall_detector.check()
+            self.border()
+            self.fsm.execute_action()
+                
+    def choose_action(self):
+        self.motions['Shove'].play()
+        if self.opponent_position.average > -0.4 and self.opponent_position.average < 0.4:
+            self.current_motion.set(self.motions['Forwards50'])
+        elif self.opponent_position.average < -0.4:
+            self.current_motion.set(self.motions['TurnLeft'])
+        elif self.opponent_position.average > 0.4:
+            self.current_motion.set(self.motions['TurnRight'])
+        else:
+            # dodging by alternating between left and right side steps to avoid easily falling off the ring
+            if self.dodging_direction == 'left':
+                if self.counter < self.NUMBER_OF_DODGE_STEPS:
+                    self.current_motion.set(self.motions['SideStepLeft'])
+                    self.counter += 1
+                else:
+                    self.dodging_direction = 'right'
+            elif self.dodging_direction == 'right':
+                if self.counter > 0:
+                    self.current_motion.set(self.motions['SideStepRight'])
+                    self.counter -= 1
+                else:
+                    self.dodging_direction = 'left'
+            else:
+                return
+        self.fsm.transition_to('BLOCKING_MOTION')
 
+    def pending(self):
+        # waits for the current motion to finish before doing anything else
+        if self.current_motion.is_over():
+            self.fsm.transition_to('CHOOSE_ACTION')
 
-wrestler = Wrestler()
+    def _get_normalized_opponent_horizontal_position(self):
+        """Returns the horizontal position of the opponent in the image, normalized to [-1, 1]
+            and sends an annotated image to the robot window."""
+        img = self.camera.get_image()
+        largest_contour, vertical, horizontal = self.locate_opponent(img)
+        output = img.copy()
+        if largest_contour is not None:
+            cv2.drawContours(output, [largest_contour], 0, (255, 255, 0), 1)
+            output = cv2.circle(output, (horizontal, vertical), radius=2,
+                                color=(0, 0, 255), thickness=-1)
+        self.camera.send_to_robot_window(output)
+        if horizontal is None:
+            return 0
+        return horizontal * 2 / img.shape[1] - 1
+
+    def locate_opponent(self, img):
+        """Image processing demonstration to locate the opponent robot in an image."""
+        # we suppose the robot to be located at a concentration of multiple color changes (big Laplacian values)
+        laplacian = cv2.Laplacian(img, cv2.CV_8U, ksize=3)
+        # those spikes are then smoothed out using a Gaussian blur to get blurry blobs
+        blur = cv2.GaussianBlur(laplacian, (0, 0), 2)
+        # we apply a threshold to get a binary image of potential robot locations
+        gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+        _, thresh = cv2.threshold(gray, 80, 255, cv2.THRESH_BINARY)
+        # the binary image is then dilated to merge small groups of blobs together
+        closing = cv2.morphologyEx(
+            thresh, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_RECT, (15, 15)))
+        # the robot is assumed to be the largest contour
+        largest_contour = IP.get_largest_contour(closing)
+        if largest_contour is not None:
+            # we get its centroid for an approximate opponent location
+            vertical_coordinate, horizontal_coordinate = IP.get_contour_centroid(
+                largest_contour)
+            return largest_contour, vertical_coordinate, horizontal_coordinate
+        else:
+            # if no contour is found, we return None
+            return None, None, None
+    def border(self):
+        imgB = self.camera_bottom.get_image()
+        output = imgB.copy()
+        #print('border function initiated')       
+        self.border_detector.avoid_line(output)
+
+# create the Robot instance and run main loop
+wrestler = Eve()
 wrestler.run()
